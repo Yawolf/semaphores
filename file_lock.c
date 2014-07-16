fdedbe2d056ecec6b3a4972d4a8160f4a71b1fc4
Santiago Cervantes <santiago.cervantes@dello.imdea>
2014-07-16 10:48:39 +0200

    Implemented funtion create_lock and the predicate create_lock. Implem…

--- file_lock.c
+++ file_lock.c
@@ -8,43 +8,22 @@
 #include <time.h>
 
 #define LOCK '1'
-#define UNLOCK '2'
+#define UNLOCK '0'
 
 char lock_name [100];
 
-int create_lock (char * file_name) {
+/** This function creates the lock named .$file_to_lock.lck  */
+
+void create_lock (char * file_name) {
   FILE * fd;
-  DIR * curr_dir;
-  char path_cdir [100]; /* a name with size 100 chars */
-  struct dirent *dir;  
+  snprintf(lock_name,100,".%s.lck",file_name); /* save in lock_name the name of the lock */
 
-  snprintf(lock_name,100,".%s.lock",file_name);
+  fd = fopen (lock_name,"ab+"); /* open the lock with the flags ab+, it means, Create or append */
 
-  if (getcwd(path_cdir,100) == NULL) {
-    perror("getcwd");
-    return -1;
-  }
+  if (fd == NULL) { fprintf(stderr,"cannot create lock\n"); return; } /* if fopen fails, cannot create the lock */
+  else fputc(UNLOCK,fd); /* otherway set free the lock writing 0 inside */
 
-  if ((curr_dir = opendir(path_cdir)) == NULL) {
-    perror("opendir");
-    return -2;
-  }
-  
-  while ((dir = readdir(curr_dir)) != NULL) {
-    if (dir->d_name[0] != '.') continue;
-    else 
-      if (!strcmp(dir->d_name,lock_name)) { printf ("%s\n",dir->d_name); return 0; }
-      else continue;
-  }
-  
-  fd = fopen (lock_name,"ab+");
-
-  if (fd == NULL) return -4;
-  else fputc(UNLOCK,fd);
-
-  fclose (fd);
-  
-  return 0;
+  fclose (fd); /* close lock */
 }
 
 /*At first, this function comproves if exists a file lock, if it exists try to take it.
@@ -53,28 +32,30 @@ If the lock is free can take it, in another way the process waits till the lock 
 void file_lock (char * file) {
   FILE * fd;
   int lock_value;
-  struct timespec timer;
+  struct timespec timer; /* struct necessary for nanosleep */
   timer.tv_sec = 0;
-  timer.tv_nsec = 100000000;
-  int ret = create_lock(file);
+  timer.tv_nsec = 100000000; /* 100000000 nsec = 0.1 sec */
+  int ret = exists_lock(file); /* comproves if the lock exists */
 
   if (ret < 0) {
-    fprintf (stderr,"cannot create lock");
+    fprintf (stderr,"this lock does not exists\n"); /* a return value < 0 means that the file doesn't exist */ 
     return;
   } 
   
-  fd = fopen (lock_name, "r+");
+  fd = fopen (lock_name, "r+"); /* Open the lock in READ/WRITE mode */
   
   if (fd == NULL) {
     perror("open");
     return;
   }
 prove:
-  lock_value = fgetc(fd);
+  lock_value = fgetc(fd); /* take the first character */
   if (lock_value == UNLOCK) { fd = freopen(NULL,"w+",fd); fputc(LOCK,fd); }
+  /* if the car == 0 then the process can take the lock setting it at 1 */
   else { printf("Nanos!!\n"); nanosleep (&timer, &timer); goto prove; }
+  /* if the lock is not free the process waits and trys again after 0.1 seconds */
 
-  fclose(fd);
+  fclose(fd); /* close the file */
 }
 
 /** This function is so easy, the process release the lock */
@@ -82,25 +63,69 @@ prove:
 void file_unlock (char * file) {
   FILE * fd;
   int lock_value;
-  
+  int ret = exists_lock(file); /* comproves if the lock exists */
+
+  if (ret < 0) {
+    fprintf (stderr,"this lock does not exists.\n"); /* a return value < 0 means that the file doesn't exist */ 
+    return;
+  }
+
   fd = fopen (lock_name,"r+");
+  
   if (fd == NULL) {
     perror("open");
     return;
   }
   lock_value = fgetc(fd);
   if (lock_value == LOCK) { fd = freopen(NULL,"w+",fd); fputc(UNLOCK,fd); }
+  /* If the lock is taken, to release it, the process overwrites the inside value, 1 to 0 */
   else fprintf(stderr,"lock already released");
+  
+  fclose(fd);
 }
+
+/* This function delete the lock assigned to a file */
 
 void clear_lock (char * file) {
   FILE * fd;
   int lock_value;
+  int ret = exists_lock(lock_name);
+
+  if (ret < 0) {
+    fprintf (stderr,"this lock does not exist.\n");
+    return;
+  }
+  
   if ((fd = fopen (lock_name,"r+")) == NULL) {
     perror("open");
     return;
   }
-  lock_value = fgetc(fd);
-  printf("%c",lock_value);
+  
+  lock_value = fgetc(fd); 
+  /* comprove if the lock has been released  and delete it*/
   (lock_value == UNLOCK) ? remove (lock_name) : fprintf(stderr,"cannot remove lock, still in use");
+}
+
+int exists_lock (char * file_name) {
+  DIR * curr_dir;
+  char path_cdir [100]; /* a name with size 100 chars */
+  struct dirent *dir;  
+
+  if (getcwd(path_cdir,100) == NULL) {
+    perror("getcwd");
+    return -1;
+  }
+  
+  if ((curr_dir = opendir(path_cdir)) == NULL) {
+    perror("opendir");
+    return -1;
+  }
+  
+  while ((dir = readdir(curr_dir)) != NULL) {
+    if (dir->d_name[0] != '.') continue;
+    else 
+      if (!strcmp(dir->d_name,lock_name)) { printf ("%s\n",dir->d_name); return 0; }
+      else continue;
+  }
+  return -2;
 }
--- file_lock.pl
+++ file_lock.pl
@@ -1,5 +1,6 @@
-:- module(file_lock, [file_lock/1, file_unlock/1, clear_lock/1], [foreign_interface]).
+:- module(file_lock, [create_lock/1, file_lock/1, file_unlock/1, clear_lock/1], [foreign_interface]).
 
+:- true pred create_lock(in(FILE)) :: atm + (foreign).
 :- true pred file_lock(in(FILE)) :: atm  + (foreign).
 :- true pred file_unlock(in(FILE)) :: atm + (foreign).
 :- true pred clear_lock(in(FILE)) :: atm + (foreign).
