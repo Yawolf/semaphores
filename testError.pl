a9af9feb6dc7a7602debb2628f0ce3e6938723d3
yawolf <yagowolf47@gmail.com>
2014-07-24 17:03:56 +0200

    deleted duplicated files

--- testError.pl
+++
@@ -1,7 +0,0 @@
-:- module(testError, [main/0]).
-:- use_module(library(strings)).
-:- use_module(library(file_utils)).
-
-main :-
-        file_to_string('fdsa.txt',String),
-        write_string(String).
--- testNoLock.pl
+++
@@ -1,7 +0,0 @@
-:- module(testNoLock, [main/0]).
-:- use_module(library(strings)).
-
-main :-
-        open('fdsa.txt',append,Stream),
-        write_string(Stream,":D\n"),
-        close(Stream).
