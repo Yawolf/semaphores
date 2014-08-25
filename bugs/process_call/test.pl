d31ada952b5308fef89057df5e3f5efcfc00f0db
Santiago Cervantes <yagowolf47@gmail.com>
2014-08-25 12:32:31 +0200

    semaphores/bugs/process_call/test.pl reordered

--- bugs/process_call/test.pl
+++ bugs/process_call/test.pl
@@ -4,6 +4,23 @@
 :- use_module(library(process)).
 :- use_module(library(strings)).
 
+:- export(test_error/1).
+test_error(0) :- !,false.
+test_error(Number) :-
+        process_call(path(ciaoc),[subsNoLock],[]),
+        process_call(path(ciaoc),[sumNoLock],[]),
+        test_error_(Number,[]).
+        
+:-export(test_error_/2).
+test_error_(0,_).
+test_error_(Number,List) :-
+        process_call(sumNoLock,[],[background(P1)]),
+        process_call(subsNoLock,[],[background(P2)]),
+        Number2 is Number-1,
+        test_error_(Number2,List),
+        insert(P1,List,List2), insert(P2,List2,List3),
+        foreach_join(List3).
+
 :- export(insert/3).
 insert(X,[],[X]).
 insert(X,[A|L],[A|L1]) :- insert(X,L,L1).
@@ -25,20 +42,3 @@ number_to_file(Stream,Number) :-
         number_codes(Number,Code),
         write_string(Stream,Code),
         write_string(Stream,"\n").
-
-:- export(test_error/1).
-test_error(0) :- !,false.
-test_error(Number) :-
-        process_call(path(ciaoc),[subsNoLock],[]),
-        process_call(path(ciaoc),[sumNoLock],[]),
-        test_error_(Number,[]).
-        
-:-export(test_error_/2).
-test_error_(0,_).
-test_error_(Number,List) :-
-        process_call(sumNoLock,[],[background(P1)]),
-        process_call(subsNoLock,[],[background(P2)]),
-        Number2 is Number-1,
-        test_error_(Number2,List),
-        insert(P1,List,List2), insert(P2,List2,List3),
-        foreach_join(List3).
